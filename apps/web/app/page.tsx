9994d88313a7b64d8e12884d0e047bc2a7cf9ffe
Debasish Raut <migrate.pascal@gmail.com>
2025-08-07 14:39:01 +0530

    added turborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@workspace/ui/components/button"
-
+import { add } from "@workspace/math/add";
+import { subtract } from "@workspace/math/subtract";
+import { Input } from "@workspace/ui/components/input";
 export default function Page() {
+  const a = 2;
+  const b = 4;
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">Hello World From the Web</h1>
+        <Button size="sm">{add({ a, b })}</Button>
+        <Input />
+        <Button size="sm">{subtract({ a, b })}</Button>
       </div>
     </div>
   )
---
+++ packages/math/src/subtract.ts
@@ -0,0 +1,3 @@
+export const subtract = ({ a, b }: { a: number, b: number }) => {
+    return a - b;
+}
